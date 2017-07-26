32b4e2681afa44704ae6687b69fc306f12ace8b8
Scott Tillman <Scotttillman23@gmail.com>
2017-07-25 20:26:16 -0500

    jquery

---
+++ DisappearingNinja.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    $("#grad1").click(function() {
+        $("#grad1").hide("slow");
+
+    });
+
+    $("#grad2").click(function() {
+        $("#grad2").hide("slow");
+
+    });
+
+    $("#grad3").click(function() {
+        $("#grad3").hide("slow");
+
+    });
+
+    $("#grad4").click(function() {
+        $("#grad4").hide("slow");
+
+    });
+
+    $("#grad5").click(function() {
+        $("#grad5").hide("slow");
+
+    });
+
+    $("#grad6").click(function() {
+        $("#grad6").hide("slow");
+
+    });
+
+    $("#grad7").click(function() {
+        $("#grad7").hide("slow");
+
+    });
+
+    $("#grad8").click(function() {
+        $("#grad8").hide("slow");
+
+    });
+
+    $("button").click(function() {
+        $("#grad1").show("slow");
+        $("#grad2").show("slow");
+        $("#grad3").show("slow");
+        $("#grad4").show("slow");
+        $("#grad5").show("slow");
+        $("#grad6").show("slow");
+        $("#grad7").show("slow");
+        $("#grad8").show("slow");
+        $("#grad9").show("slow");
+
+    });
+})
